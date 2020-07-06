e96da7854a0b85fcd3e8a07c01b12e0571fac402
fabricio4077 <orejona2501>
2020-07-06 13:25:40 -0500

    cronograma

--- grails-app/domain/proyectos/Cronograma.groovy
+++ grails-app/domain/proyectos/Cronograma.groovy
@@ -25,6 +25,10 @@ class Cronograma   {
      */
     Fuente fuente
     /**
+     * Fuente del cronograma 2
+     */
+    Fuente fuente2
+    /**
      * Año del cronograma
      */
     Anio anio
@@ -64,11 +68,12 @@ class Cronograma   {
             mes column: 'mess__id'
             marcoLogico column: 'mrlg__id'
             fuente column: 'fnte__id'
+            fuente2 column: 'fnte2_id'
             anio column: 'anio__id'
             presupuesto column: 'prsp__id'
             presupuesto2 column: 'prsp2_id'
-            valor column: 'messvlor'
-            valor2 column: 'mesvlor2'
+            valor column: 'crngvlor'
+            valor2 column: 'crngvl02'
         }
     }
 
@@ -79,6 +84,7 @@ class Cronograma   {
         mes(blank: true, nullable: true, attributes: [mensaje: 'Mes'])
         marcoLogico(blank: true, nullable: true, attributes: [mensaje: 'Actividad del marco lógico'])
         fuente(blank: true, nullable: true, attributes: [mensaje: 'Fuente'])
+        fuente2(blank: true, nullable: true, attributes: [mensaje: 'Fuente2'])
         anio(blank: true, nullable: true, attributes: [mensaje: 'Año'])
         presupuesto(nullable: false, blank: false)
         presupuesto2(nullable: true, blank: true)
